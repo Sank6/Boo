26498998e191352350224b692dbc86c6be2187f0
14ROVI <29734170+14ROVI@users.noreply.github.com>
2021-10-30 11:30:29 +0100

    template

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import pygame
 
+FRAMERATE = 60
+GAMERUNNING = True
+
+def loop():
+    global GAMERUNNING
+    
+    while GAMERUNNING:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                GAMERUNNING = False
+
+
 
 def main():
-    print("ok")
+    (width, height) = (640, 480)
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.flip()
+    loop()
 
 
 if __name__ == "__main__":
