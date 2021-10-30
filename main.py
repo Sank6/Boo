92c94ec4ded41d3397dcb31e989d2aa560f79134
14ROVI <29734170+14ROVI@users.noreply.github.com>
2021-10-30 14:59:27 +0100

    finished title screen

--- main.py
+++ main.py
@@ -2,45 +2,72 @@ import pygame
 from pygame.locals import *
 import time
 
-FRAMERATE = 60
-GAMERUNNING = True
+BUTTON_TEXT_COLOUR = (64, 28, 20)
+## none button
+BUTTON_OUTLINE_0 = (162, 88, 69)
+BUTTON_OUTLINE_1 = (102, 33, 64)
+BUTTON_OUTLINE_2 = (102, 56, 45)
+BUTTON_OUTLINE_3 = (162, 88, 69)
+BUTTON_OUTLINE_SHADOW = (64, 28, 20)
+BUTTON_MAIN = (210, 148, 101)
 
-# Colours
-orange = (245, 57, 0)
-light = (230, 230, 230)
-dark = (9, 0, 33)
-purple = (68, 0, 255)
+## pressed Button
+BUTTON_PRESSED_OUTLINE_0 = (117, 46, 55)
+BUTTON_PRESSED_OUTLINE_1 = (210, 148, 101)
+BUTTON_PRESSED_MAIN = (162, 88, 69)
+
+## hovered button
+BUTTON_HOVERED_MAIN = (207, 157, 122)
+
+pygame.init()
 
 class Game:
     def __init__(self, s_width, s_height):
-        self.screen = pygame.display.set_mode((s_width, s_height))
+        self.screen = pygame.display.set_mode((s_width, s_height), flags=pygame.SCALED)
         self.player = None
         self.level = 1
         self.paused = False
         self.running = True
         self.all_sprites = pygame.sprite.Group()
+        self.clock = pygame.time.Clock()
+        self.background = pygame.image.load("assets/title_background.png")
+
         self.startTitleScreen()
 
     def start(self):
         self.control_loop()
 
     def startTitleScreen(self):
-        playButton = Button(self, 10, 10, 100, 50, dark, "play", self.play)
+        playButton = Button(self, 65, 86, 111, 25, "NEW GAME", self.play)
+        playButton = Button(self, 65, 115, 111, 25, "HI-SCORES", self.play)
+        quitButton = Button(self, 80, 145, 80, 20, "QUIT", self.quit)
+
+
+    def quit(self, button, event, game):
+        self.running = False
 
     def play(self, button, event, game):
         pass
 
     def control_loop(self):
         while self.running:
+            self.screen.blit(self.background, (0, 0))
+
             self.all_sprites.update()
-            print(self.all_sprites)
             for event in pygame.event.get():
-                #print(event)
                 if event.type == pygame.QUIT:
                     self.running = False
+            for sprite in self.all_sprites:
+                if isinstance(sprite, Button):
+                    sprite.check_state(event)
+                sprite.draw()
+            self.update()
+
+        pygame.quit()
 
     def update(self):
         pygame.display.flip()
+        self.clock.tick(60)
 
 """
 game = Game(640, 480)
@@ -51,7 +78,7 @@ game.all_sprites.add(player)
 """
 #keeping track of ghost time
 start_time = time.start_time
-while start_time != 10:
+while start_time != 600:
     keep running game
 """
 
@@ -62,10 +89,15 @@ quitButton = Button(100, 100, 100, 100, black, "Quit Game", quit)
 startButton = Button(100, 100, 100, 100, black, "Join Game", start)
 """
 
-"""
-quit():
-    exit;
-"""
+class Box(pygame.sprite.Sprite):
+    def __init__(self, x_position, y_position, width, height, colour, text):
+        self.x = x_position
+        self.y = y_position
+        self.width = width
+        self.height = height
+        self.colour = colour
+        self.text = text
+        self.font = pygame.font.Font("assets/font.ttf", height-10)
 
 
 class Player(pygame.sprite.Sprite):
@@ -75,8 +107,9 @@ class Player(pygame.sprite.Sprite):
         self.yPosition = y
         self.keys = []
 
+
 class Button(pygame.sprite.Sprite):
-    def __init__(self, game, x, y, width, height, colour, text, callback):
+    def __init__(self, game, x, y, width, height, text, callback):
         pygame.sprite.Sprite.__init__(self)
         game.all_sprites.add(self)
 
@@ -85,21 +118,62 @@ class Button(pygame.sprite.Sprite):
         self.y = y
         self.width = width
         self.height = height
-        self.colour = colour
         self.text = text
         self.callback = callback
+        self.font = pygame.font.Font("assets/font.ttf", height-14)
+
+        self.state = "none" # "none", "hovered", "pressed"
 
     def draw(self):
-        pygame.draw.rect(self.game.screen, self.colour, [self.width/2,self.height/2,x,y])
+        screen = self.game.screen
+        if self.state == "none":
+            pygame.draw.rect(screen, BUTTON_OUTLINE_0, [self.x,self.y+1,self.width,self.height-2])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_0, [self.x+1,self.y, self.width-2,self.height])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_1, [self.x+1,self.y+1, self.width-2,self.height-2])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_2, [self.x+2,self.y+2,self.width-4,self.height-4])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_3, [self.x+3,self.y+3,self.width-6,self.height-6])
 
-    def check_clicked(self, event):
-        mouse = dict(event)["pos"]
-        if self.width/2 <= mouse[0] <= self.width/2+x and self.height/2 <= mouse[1] <= self.height/2+y:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                click(event)
-            else:
-                pass
-                # Hover event
+            pygame.draw.rect(screen, BUTTON_MAIN, [self.x+4,self.y+4,self.width-8,self.height-8])
+
+            pygame.draw.rect(screen, BUTTON_OUTLINE_SHADOW, [self.x+3,self.y+self.height-3,self.width-4,2])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_SHADOW, [self.x+self.width-3,self.y+4,2,self.height-5])
+
+        elif self.state == "hovered":
+            pygame.draw.rect(screen, BUTTON_OUTLINE_0, [self.x,self.y+1,self.width,self.height-2])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_0, [self.x+1,self.y, self.width-2,self.height])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_1, [self.x+1,self.y+1, self.width-2,self.height-2])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_2, [self.x+2,self.y+2,self.width-4,self.height-4])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_3, [self.x+3,self.y+3,self.width-6,self.height-6])
+
+            pygame.draw.rect(screen, BUTTON_HOVERED_MAIN, [self.x+4,self.y+4,self.width-8,self.height-8])
+
+            pygame.draw.rect(screen, BUTTON_OUTLINE_SHADOW, [self.x+3,self.y+self.height-3,self.width-4,2])
+            pygame.draw.rect(screen, BUTTON_OUTLINE_SHADOW, [self.x+self.width-3,self.y+4,2,self.height-5])
+
+        elif self.state == "pressed":
+            pygame.draw.rect(screen, BUTTON_PRESSED_OUTLINE_0, [self.x+1,self.y+2,self.width-2,self.height-4])
+            pygame.draw.rect(screen, BUTTON_PRESSED_OUTLINE_0, [self.x+2,self.y+1, self.width-4,self.height-2])
+            pygame.draw.rect(screen, BUTTON_PRESSED_OUTLINE_1, [self.x+2,self.y+2, self.width-4,self.height-4])
+
+            pygame.draw.rect(screen, BUTTON_PRESSED_MAIN, [self.x+3,self.y+3,self.width-6,self.height-6])
+
+        text = self.font.render(self.text, True, BUTTON_TEXT_COLOUR)
+        text_rect = text.get_rect(center=(self.x+self.width/2, self.y+self.height/2))
+        screen.blit(text, text_rect)
+
+    def check_state(self, event):
+        mouse = pygame.mouse.get_pos()
+        if self.x <= mouse[0] <= self.x+self.width and self.y <= mouse[1] <= self.y+self.height:
+            if event.type == pygame.MOUSEBUTTONUP:
+                if self.state == "pressed":
+                    self.click(event)
+                self.state = "hovered"
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.state = "pressed"
+            elif self.state == "none":
+                self.state = "hovered"
+        else:
+            self.state = "none"
 
 
     def click(self, event):
@@ -108,7 +182,7 @@ class Button(pygame.sprite.Sprite):
 
 
 def main():
-    game = Game(640, 480)
+    game = Game(240, 180)
     game.start()
 
 
